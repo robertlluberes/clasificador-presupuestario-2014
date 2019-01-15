a345eb74d163a66b311374ce18baed2366655743
Robert Lluberes <robertlluberes@gmail.com>
2019-01-14 23:03:56 -0400

    Corrigiendo estilo en el script

--- clasificador-tipo-moneda/mssql-clasificador-modena.sql
+++ clasificador-tipo-moneda/mssql-clasificador-modena.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Monedas]
 (
-	[MonedaID]				tinyint			PRIMARY KEY	IDENTITY(1, 1),
+	[MonedaID]			tinyint			PRIMARY KEY	IDENTITY(1, 1),
 	[CodigoMoneda]			tinyint			NOT NULL,
 	[SiglaMoneda]			varchar(3)		NOT NULL,
 	[CodigoAlfabetico]		varchar(3)		NOT NULL,
@@ -14,18 +14,18 @@ GO
 
 INSERT INTO [dbo].[Monedas]
 VALUES
-	(15,'VEB','VEB','BOLÕVARES'),
-	(21,'BV$','BV$','BOLÕVARES VENEZUELA EQUIV. US$'),
+	(15,'VEB','VEB','BOL√çVARES'),
+	(21,'BV$','BV$','BOL√çVARES VENEZUELA EQUIV. US$'),
 	(17,'BRC','BRC','BRASIL, REAL'),
 	(20,'ATS','ATS','CHELINES AUSTRIACOS'),
 	(14,'SEK','SEK','CORONAS SUECAS'),
 	(22,'CB$','CB$','CRUZEIRO BRAZIL EQUIV. US$'),
 	(13,'SDR','SDR','DERECHOS ESPECIALES DE GIRO'),
-	(33,'DEG','DEU','DEU (Derechos especiales de giro equivalente a DÛlares)'),
+	(33,'DEG','DEU','DEU (Derechos especiales de giro equivalente a D√≥lares)'),
 	(18,'DKK','DKK','DINAMARCA, CORONA DANESA'),
 	(28,'YUD','YUD','DINARES YUGOSLAVOS'),
-	(5,'CAD','CAD','D”LARES CANADIENSES'),
-	(2,'USD','USD','D”LARES ESTADOUNIDENSES'),
+	(5,'CAD','CAD','D√ìLARES CANADIENSES'),
+	(2,'USD','USD','D√ìLARES ESTADOUNIDENSES'),
 	(23,'GRD','GRD','DRACMAS GRIEGAS'),
 	(3,'EUR','EUR','EUROS'),
 	(25,'NLG','NLG','FLORINES NEERLANDESES'),
@@ -37,7 +37,7 @@ VALUES
 	(10,'ITL','ITL','LIRAS ITALIANAS'),
 	(7,'DEM','DEM','MARCOS ALEMANES'),
 	(19,'NOK','NOK','NORUEGA, CORONA NORUEGA'),
-	(8,'ESP','ESP','PESETAS ESPA—OLAS'),
+	(8,'ESP','ESP','PESETAS ESPA√ëOLAS'),
 	(27,'PA$','PA$','PESOS ARGENTINOS EQUIV. USD$'),
 	(1,'RD$','DOP','PESOS DOMINICANOS'),
 	(12,'PD$','PD$','PESOS DOMINICANOS EQUIV. US'),
@@ -46,7 +46,7 @@ VALUES
 	(30,'UAE','UAE','UAC (Equivalente a euros)'),
 	(31,'UAF','UAF','UAC (Equivalente a francos suizos)'),
 	(32,'UAJ','UAJ','UAC (Equivalente a yenes)'),
-	(35,'UAD','UAD','UAC (equivalente a dÛlares)'),
+	(35,'UAD','UAD','UAC (equivalente a d√≥lares)'),
 	(24,'IDB','IDB','UNIDAD DE CUENTA BID'),
 	(11,'JPY','JPY','YENES');
 GO
