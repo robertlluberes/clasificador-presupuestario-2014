a6f1294b5773534207b47102947f8f140dabcebc
Robert Lluberes <robertlluberes@gmail.com>
2019-01-14 23:03:21 -0400

    Corrigiendo estilo en el script

--- clasificador-origen-contrataciones-paises/mssql-clasificador-origen-contratation-paises.sql
+++ clasificador-origen-contrataciones-paises/mssql-clasificador-origen-contratation-paises.sql
@@ -2,24 +2,24 @@ CREATE TABLE [dbo].[Paises]
 (
 	[PaisID]				tinyint			PRIMARY KEY	IDENTITY(1, 1),
 	[Codigo]				varchar(3)		NOT NULL,
-	[CodigoSIGADE]			varchar(3)		NOT NULL,
+	[CodigoSIGADE]				varchar(3)		NOT NULL,
 	[Nombre]				varchar(64)		NOT NULL
 
-	CONSTRAINT UQ_Paises_Codigo			UNIQUE ([Codigo]),
+	CONSTRAINT UQ_Paises_Codigo		UNIQUE ([Codigo]),
 	CONSTRAINT UQ_Paises_CodigoSIGADE	UNIQUE ([CodigoSIGADE]),
-	CONSTRAINT UQ_Paises_Nombre			UNIQUE ([Nombre]),
+	CONSTRAINT UQ_Paises_Nombre		UNIQUE ([Nombre]),
 );
 GO
 
 INSERT INTO [dbo].[Paises]
 VALUES
-	('01','DO','REP⁄BLICA DOMINICANA'),
+	('01','DO','REP√öBLICA DOMINICANA'),
 	('02','AR','ARGENTINA'),
-	('03','BE','B…LGICA'),
+	('03','BE','B√âLGICA'),
 	('04','BO','BOLIVIA'),
 	('05','BR','BRASIL'),
 	('06','BS','BAHAMAS'),
-	('07','CA','CANAD¡'),
+	('07','CA','CANAD√Å'),
 	('08','CH','SUIZA'),
 	('09','CL','CHILE'),
 	('10','CN','REP. POPULAR DE CHINA'),
@@ -28,9 +28,9 @@ VALUES
 	('13','DE','REP. FED. DE ALEMANIA'),
 	('14','DK','DINAMARCA'),
 	('15','EC','ECUADOR'),
-	('16','ES','ESPA—A'),
+	('16','ES','ESPA√ëA'),
 	('17','FR','FRANCIA'),
-	('18','GB','GRAN BRETA—A'),
+	('18','GB','GRAN BRETA√ëA'),
 	('19','IE','IRLANDA'),
 	('20','IL','ISRAEL'),
 	('21','IT','ITALIA'),
@@ -38,17 +38,14 @@ VALUES
 	('23','KR','REPUBLICA DE COREA'),
 	('24','LU','LUXEMBURGO'),
 	('25','MS','MONTSERRAT'),
-	('26','MX','M…XICO'),
+	('26','MX','M√âXICO'),
 	('27','NI','NICARAGUA'),
 	('28','NL','HOLANDA'),
 	('29','NO','NORUEGA'),
-	('30','PA','PANAM¡'),
-	('31','PE','PER⁄'),
+	('30','PA','PANAM√Å'),
+	('31','PE','PER√ö'),
 	('32','PR','PUERTO RICO'),
 	('33','PY','PARAGUAY'),
 	('34','SE','SUECIA'),
 	('35','SV','EL SALVADOR');
 GO
-
-SELECT *
-FROM Paises
